78fa60a39185b0e705fc369e959c57853bbeb31e
Amannnnnn06 <amannnnnn1405@gmail.com>
2024-08-30 20:29:50 +0530

    Add files via upload

---
+++ printarray2.cpp
@@ -0,0 +1,26 @@
+//Name:Aman Prajapati
+//PRN:23070123017
+//Aim:Implemention of Arrays and Strings to print the given Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+ int x[6] = {19,10,8,6,8};
+ for(int i=0;i<5;i++)
+ {
+    cout << x[i];
+    cout<< endl;
+ }
+
+    return 0;
+}
+
+
+/*Output:
+19
+10
+8
+6
+8*/
